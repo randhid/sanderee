1ed701b80088d8094bf44c5dac3f0e3b85a36962
randhid <rand@viam.com>
2025-04-17 16:06:22 -0400

    add oses and geometires

--- module.go
+++ module.go
@@ -15,6 +15,8 @@ import (
 var (
 	SanderEe         = resource.NewModel("rand", "sander-ee", "sander-ee")
 	errUnimplemented = errors.New("unimplemented")
+	blockDims        = r3.Vector{X: 38, Y: 70, Z: 270}
+	totalLength      = 105.
 )
 
 func init() {
@@ -35,29 +37,45 @@ type sanderEeSanderEe struct {
 }
 
 func NewSander(ctx context.Context, deps resource.Dependencies, rawConf resource.Config, logger logging.Logger) (gripper.Gripper, error) {
+	// internal clamp
 
-	var errs error
+	// pose measured from onshape CAD defined as distance from face in contact with the ur5e end effector
+	// to the middle point of the internal clamp height dimension
 	ipose := spatialmath.NewPose(
-		r3.Vector{Z: -14.5}, &spatialmath.OrientationVectorDegrees{OY: 1},
+		r3.Vector{Z: -11}, &spatialmath.OrientationVectorDegrees{OY: 1},
 	)
-	internal, err := spatialmath.NewCapsule(ipose, 55, 300, "internal-clamp")
-	errors.Join(errs, err)
+	// internal clamp total dims are L:300mm W: 110mm H:50mm
+	// this makes a capsule to match the internal clamps length with a best fit radius
+	internal, err := spatialmath.NewCapsule(ipose, 27.5, 245, "internal-clamp")
+	if err != nil {
+		return nil, err
+	}
+
+	// pose measured from CAD defined as distance from face in contact with the ur5e end effector
+	// to furthest face of the pivot
 	ppose := spatialmath.NewPose(
 		r3.Vector{Z: 51.475}, &spatialmath.OrientationVectorDegrees{OY: 1},
 	)
-	pivot, err := spatialmath.NewCapsule(ppose, 80, 300, "pivot")
-	errors.Join(errs, err)
+	// pivot total dims are L300mm W: 110mm H:50mm
+	// this makes a capsule to match the pivot length with a best fit radius
+	pivot, err := spatialmath.NewCapsule(ppose, 40, 220, "pivot")
+	if err != nil {
+		return nil, err
+	}
+
+	// hose - ballpark placement is middle of the sanding block height block at one edge
 	hpose := spatialmath.NewPose(
-		r3.Vector{Y: 78, Z: 105}, &spatialmath.OrientationVectorDegrees{OY: 1},
+		r3.Vector{Y: blockDims.Z / 2, Z: totalLength - blockDims.X/2}, &spatialmath.OrientationVectorDegrees{OY: 1},
 	)
-	hose, err := spatialmath.NewSphere(hpose, 50, "hose")
+	hose, err := spatialmath.NewSphere(hpose, 25, "hose")
+
+	// sanding block dims are L: 270mm W: 78mm H:38mm (X)
 	bpose := spatialmath.NewPose(
-		r3.Vector{Z: 51.475}, &spatialmath.OrientationVectorDegrees{OY: 1},
+		r3.Vector{Z: totalLength}, &spatialmath.OrientationVectorDegrees{OY: 1},
 	)
-	block, err := spatialmath.NewBox(bpose, r3.Vector{X: 270, Y: 70, Z: 38}, "block")
-	errors.Join(errs, err)
-	if errs != nil {
-		return nil, errs
+	block, err := spatialmath.NewBox(bpose, blockDims, "block")
+	if err != nil {
+		return nil, err
 	}
 
 	geoms := []spatialmath.Geometry{
